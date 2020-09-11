0c99be0f111c0036c335fc4762f6b9d2e01193e1
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2020-09-11 13:46:13 -0400

    Update prs_pipeline_submission.sh

--- prs_pipeline_submission.sh
+++ prs_pipeline_submission.sh
@@ -8,4 +8,4 @@ CLUSTER_PATH=/humgen/diabetes2/users/shsu/prs_project/score_info
 VCF_PATH=/humgen/diabetes2/users/shsu/prs_project/vcf_file.vcf.gz
 PROJECT_NAME=project_name
 
-source $SCRIPT_PATH/prs_pipeline.sh -s $SCRIPT_PATH -p $PROJECT_PATH -c $CLUSTER_PATH -v $VCF_PATH -n $PROJECT_NAME
+source $SCRIPT_PATH/prs_pipeline.sh -s $SCRIPT_PATH -p $PROJECT_PATH -c $CLUSTER_PATH -f $VCF_PATH -n $PROJECT_NAME
