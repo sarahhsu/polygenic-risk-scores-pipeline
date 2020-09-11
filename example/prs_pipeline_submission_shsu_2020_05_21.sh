185ddd7c1aa220e8671762d062b5805f3ac4b680
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2020-09-11 13:05:41 -0400

    Update prs_pipeline_submission_shsu_2020_05_21.sh

--- example/prs_pipeline_submission_shsu_2020_05_21.sh
+++ example/prs_pipeline_submission_shsu_2020_05_21.sh
@@ -4,7 +4,8 @@
 #$ -pe smp 4
 #$ -binding linear:4
 #$ -l h_rt=12:30:00
-#$ -o
+#$ -o /humgen/diabetes2/users/shsu/prs_project
+#$ -e /humgen/diabetes2/users/shsu/prs_project
 
 source /broad/software/scripts/useuse
 use .bcftools-1.9
