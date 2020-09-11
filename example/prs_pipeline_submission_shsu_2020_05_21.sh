d500178910d4720be929930622b1af22a9a4ba8a
Hsu, Sarah <shsu8@mgh.harvard.edu>
2020-09-11 13:00:03 -0400

    Updates to submission examples

---
+++ example/prs_pipeline_submission_shsu_2020_05_21.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#$ -l h_vmem=80G
+#$ -pe smp 4
+#$ -binding linear:4
+#$ -l h_rt=12:30:00
+#$ -o
+
+source /broad/software/scripts/useuse
+use .bcftools-1.9
+use R-3.5
+
+
+SCRIPT_PATH=/humgen/diabetes2/users/shsu/prs_project
+PROJECT_PATH=/humgen/diabetes2/users/shsu/prs_project
+CLUSTER_PATH=/humgen/diabetes2/users/shsu/prs_project/score_info
+VCF_PATH=/humgen/diabetes2/users/shsu/prs_project/vcf_file.vcf.gz
+PROJECT_NAME=project_name
+
+source $SCRIPT_PATH/prs_pipeline.sh -s $SCRIPT_PATH -p $PROJECT_PATH -c $CLUSTER_PATH -v $VCF_PATH -n $PROJECT_NAME
--- prs_pipeline_submission.sh
+++ prs_pipeline_submission.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-#$ -l h_vmem=80G
-#$ -pe smp 4
-#$ -binding linear:4
-#$ -l h_rt=12:30:00
-#$ -o
-
-source /broad/software/scripts/useuse
-use .bcftools-1.9
-use R-3.5
-
+# Make sure you have bcftools and R modules loaded!
 
 SCRIPT_PATH=/humgen/diabetes2/users/shsu/prs_project
 PROJECT_PATH=/humgen/diabetes2/users/shsu/prs_project
 CLUSTER_PATH=/humgen/diabetes2/users/shsu/prs_project/score_info
-VCF_PATH=/humgen/diabetes2/users/josep/PartnersBIOBANK/merge_all_35K_datasets/merge_35K_genotypes/EU/imputation_HRC_EU
+VCF_PATH=/humgen/diabetes2/users/shsu/prs_project/vcf_file.vcf.gz
 PROJECT_NAME=project_name
 
 source $SCRIPT_PATH/prs_pipeline.sh -s $SCRIPT_PATH -p $PROJECT_PATH -c $CLUSTER_PATH -v $VCF_PATH -n $PROJECT_NAME
