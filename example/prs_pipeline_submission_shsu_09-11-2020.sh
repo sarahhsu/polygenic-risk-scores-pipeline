bf4f7b52ad92dec8dcd5cd8d48761a93f1d533a8
Hsu, Sarah <shsu8@mgh.harvard.edu>
2020-09-24 09:53:19 -0400

    adding full inputs and outputs for the example run

--- example/prs_pipeline_submission_shsu_09-11-2020.sh
+++ example/prs_pipeline_submission_shsu_09-11-2020.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 #$ -l h_vmem=80G
-#$ -pe smp 4
-#$ -binding linear:4
 #$ -l h_rt=12:30:00
 #$ -o /humgen/diabetes2/users/shsu/prs_project
 #$ -e /humgen/diabetes2/users/shsu/prs_project
 
+# Make sure you have bcftools and R modules loaded!
 source /broad/software/scripts/useuse
-use .bcftools-1.9
 use R-3.5
-
+use .bcftools-1.9
 
 SCRIPT_PATH=/humgen/diabetes2/users/shsu/prs_project/polygenic-risk-scores-pipeline
 PROJECT_PATH=/humgen/diabetes2/users/shsu/prs_project
 CLUSTER_PATH=/humgen/diabetes2/users/shsu/prs_project/score_info
-VCF_PATH=/humgen/diabetes2/users/shsu/prs_project/vcf_file.vcf.gz
-PROJECT_NAME=project_name
+VCF_PATH=/humgen/diabetes2/users/josep/PartnersBIOBANK/merge_all_35K_datasets/merge_35K_genotypes/EU/imputation_HRC_EU/chr22.dose.vcf.gz
+PROJECT_NAME=prs_project
 
 source $SCRIPT_PATH/prs_pipeline.sh -s $SCRIPT_PATH -p $PROJECT_PATH -c $CLUSTER_PATH -f $VCF_PATH -n $PROJECT_NAME
