e9cea37f07e90559e3c2f1d4ca75776af50d8321
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2021-03-22 12:57:11 -0400

    Update vcf_pipeline.sh

--- vcf_pipeline.sh
+++ vcf_pipeline.sh
@@ -78,8 +78,11 @@ then
   cd $MERGED_VCF_FOLDER
   for file in $VCF_PATH/*.vcf.gz; do
     filename=$(basename $file .vcf.gz)
-    tabix -p vcf $file
-    bcftools view -O v -R $PROJECT_FOLDER/snps.txt $file  | bcftools annotate --output-type z --output ${filename}_snps_${USER}_${DATE}.recode.vcf.gz  -I '%CHROM:%POS:%REF:%ALT'
+        gunzip -c $file | awk '{gsub(/^chr/,""); print}' > temp.vcf
+    bgzip temp.vcf
+    tabix -p vcf temp.vcf.gz
+    bcftools view -O v -R $PROJECT_FOLDER/snps.txt temp.vcf.gz | bcftools annotate --output-type z --output ${filename}_snps_${USER}_${DATE}.recode.vcf.gz  -I '%CHROM:%POS:%REF:%ALT'
+    rm *temp.vcf.gz*
   done
 
   wait
