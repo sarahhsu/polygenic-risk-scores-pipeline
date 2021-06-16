ee0103fe4a18119638eb778b8381f6f09c588eb1
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2021-06-16 09:39:54 -0400

    Update prs_pipeline_submission.sh

--- prs_pipeline_submission.sh
+++ prs_pipeline_submission.sh
@@ -2,10 +2,10 @@
 
 # Make sure you have bcftools, tabix, and R modules loaded!
 
-SCRIPT_PATH=/humgen/diabetes2/users/shsu/prs_project/polygenic-risk-scores-pipeline
-PROJECT_PATH=/humgen/diabetes2/users/shsu/prs_project
-CLUSTER_PATH=/humgen/diabetes2/users/shsu/prs_project/score_info
-VCF_PATH=/humgen/diabetes2/users/shsu/prs_project/vcf_file.vcf.gz
+SCRIPT_PATH=/prs_project/polygenic-risk-scores-pipeline
+PROJECT_PATH=/prs_project
+CLUSTER_PATH=/prs_project/score_info
+VCF_PATH=/prs_project/vcf_file.vcf.gz
 PROJECT_NAME=project_name
 
 source $SCRIPT_PATH/prs_pipeline.sh -s $SCRIPT_PATH -p $PROJECT_PATH -c $CLUSTER_PATH -f $VCF_PATH -m $PROJECT_NAME
