e79af92b15b07a00688114ae59961c2952c1be2b
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2021-06-17 18:24:13 -0400

    Update prs_pipeline_submission_shsu_09-11-2020.sh

--- example/prs_pipeline_submission_shsu_09-11-2020.sh
+++ example/prs_pipeline_submission_shsu_09-11-2020.sh
@@ -2,8 +2,8 @@
 
 #$ -l h_vmem=80G
 #$ -l h_rt=12:30:00
-#$ -o /humgen/diabetes2/users/shsu/prs_project
-#$ -e /humgen/diabetes2/users/shsu/prs_project
+#$ -o /prs_project/outs
+#$ -e /prs_project/outs
 
 # Make sure you have bcftools and R modules loaded!
 source /broad/software/scripts/useuse
@@ -13,7 +13,7 @@ use .bcftools-1.9
 SCRIPT_PATH=/prs_project/polygenic-risk-scores-pipeline
 PROJECT_PATH=/prs_project
 CLUSTER_PATH=/prs_project/score_info
-VCF_PATH=vcf.dose.vcf.gz
+VCF_PATH=prs_project/data/vcf.dose.vcf.gz
 PROJECT_NAME=prs_project
 
 source $SCRIPT_PATH/prs_pipeline.sh -s $SCRIPT_PATH -p $PROJECT_PATH -c $CLUSTER_PATH -f $VCF_PATH -m $PROJECT_NAME
