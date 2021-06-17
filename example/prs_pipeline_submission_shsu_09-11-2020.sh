3996b762e1ce6210787825ca22cf4cf3014150ea
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2021-06-17 17:54:11 -0400

    Update prs_pipeline_submission_shsu_09-11-2020.sh

--- example/prs_pipeline_submission_shsu_09-11-2020.sh
+++ example/prs_pipeline_submission_shsu_09-11-2020.sh
@@ -10,10 +10,10 @@ source /broad/software/scripts/useuse
 use R-3.5
 use .bcftools-1.9
 
-SCRIPT_PATH=/humgen/diabetes2/users/shsu/prs_project/polygenic-risk-scores-pipeline
-PROJECT_PATH=/humgen/diabetes2/users/shsu/prs_project
-CLUSTER_PATH=/humgen/diabetes2/users/shsu/prs_project/score_info
-VCF_PATH=/humgen/diabetes2/users/josep/PartnersBIOBANK/merge_all_35K_datasets/merge_35K_genotypes/EU/imputation_HRC_EU/chr22.dose.vcf.gz
+SCRIPT_PATH=/prs_project/polygenic-risk-scores-pipeline
+PROJECT_PATH=/prs_project
+CLUSTER_PATH=/prs_project/score_info
+VCF_PATH=vcf.dose.vcf.gz
 PROJECT_NAME=prs_project
 
 source $SCRIPT_PATH/prs_pipeline.sh -s $SCRIPT_PATH -p $PROJECT_PATH -c $CLUSTER_PATH -f $VCF_PATH -m $PROJECT_NAME
